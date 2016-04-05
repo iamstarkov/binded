c3ff137269e0bbdd8412f22c4a9c31357d350a71
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-05 17:08:35 +0200

    write tests

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
 import test from 'ava';
-import binded from './index';
+import R from 'ramda';
+import binded from './';
+import Promise from 'pinkie-promise';
 
-test('should binded', (t) =>
-  t.same(binded('unicorns'), 'unicorns'));
+test('an Object', t =>
+  t.is(R.type(binded(console)), 'Object'));
 
-test('should binded invalid input', (t) =>
-  t.same(binded(), undefined));
+test('still a function', t =>
+  t.is(R.type(binded(console).log), 'Function'));
+
+test('still a function and not throws', t =>
+  t.notThrows(() => binded(console).log(null)));
+
+test('even Promise.reject is a function', t =>
+  t.is(R.type(binded(Promise).reject), 'Function'));
+
+test('Promise.resolve still works', t =>
+  binded(Promise).resolve(true).then(i => t.true(i)));
+
+test('empty input', t => t.throws(() => { binded(); }, TypeError));
